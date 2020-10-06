a18f3e9b66d64c2e14ba5175f15765a13c50f13e
Gavin <gavinbrrtt@gmail.com>
2020-10-06 01:18:31 -0700

    Added more scss styling.

--- src/components/PropContent.jsx
+++ src/components/PropContent.jsx
@@ -11,16 +11,16 @@ const prop23Title = 'Dialysis Clinic Requirements Initiative';
 const prop24Title = "Consumer Personal Information Law and Agency Initiative";
 const prop25Title = 'Replace Cash Bash with Risk Assessment Referendum';
 
-const prop14yes = "Voting YES will allow California to sell $5.5 Billion worth of bonds to fund the California Institute for Regenerative Medicine (CIRM) which undertakes stem cell research into Alzheimer's, etc.";
-const prop15yes = 'Voting YES will split residential and commercial properties into different tax rolls. This will affect the taxes on certain businesses as well as on landlords. Revenue will go to fund local schools and governments. Small businesses and farms are exempt from this initiative';
-const prop16yes = '';
+const prop14yes = "will allow California to sell $5.5 Billion worth of bonds to fund the California Institute for Regenerative Medicine (CIRM) which undertakes stem cell research into Alzheimer's, etc.";
+const prop15yes = 'will split residential and commercial properties into different tax rolls. This will affect the taxes on certain businesses as well as on landlords. Revenue will go to fund local schools and governments. Small businesses and farms are exempt from this initiative';
+const prop16yes = "will permit homeowners who either are 1) over 55, 2) severely disabled, or 3) have had a home destroyed by a wildfire or other disaster to transfer their primary residence's property tax base value to a replacement residence of any value, anywhere in the state.";
 const prop17yes = '';
 const prop18yes = '';
-const prop19yes = 'Voting YES allows people who inherited properties to keep receiving the lowered property tax benefit of their old property.';
+const prop19yes = 'allows people who inherited properties to keep receiving the lowered property tax benefit of their old property.';
 const prop20yes = '';
-const prop21yes = 'Voting YES allows governments to pass stricter rent control measures on properties at least 15 years of age';
-const prop22yes = 'Voting YES offers workers a few new benefits, but not as many as the state law proposed to handle Uber/Lyft';
-const prop23yes = 'Voting YES will add more requirements to dialysis clinics';
+const prop21yes = 'allows governments to pass stricter rent control measures on properties at least 15 years of age';
+const prop22yes = 'offers workers a few new benefits, but not as many as the state law proposed to handle Uber/Lyft';
+const prop23yes = 'will add more requirements to dialysis clinics';
 const prop24yes = 'data privacy';
 const prop25yes = 'bail, profiling, artificial intelligence';
 
@@ -29,7 +29,7 @@ const prop15no = 'raised taxes, landlords, businesses, aid to schools';
 const prop16no = '';
 const prop17no = '';
 const prop18no = '';
-const prop19no = 'Voting NO would take away the ability to transfer your property tax break to your new property';
+const prop19no = 'would take away the ability to transfer your property tax break to your new property';
 const prop20no = '';
 const prop21no = 'rent control';
 const prop22no = 'uber, lyft, app-based work';
@@ -50,7 +50,32 @@ const prop23key = 'dialysis';
 const prop24key = 'data privacy';
 const prop25key = 'bail, profiling, artificial intelligence';
 
+const prop14Back = 'hello';
+const prop15Back = 'ther';
+const prop16Back = '';
+const prop17Back = null;
+const prop18Back = null;
+const prop19Back = null;
+const prop20Back = null;
+const prop21Back = null;
+const prop22Back = null;
+const prop23Back = null;
+const prop24Back = null;
+const prop25Back = null;
+const prop26Back = null;
 
+const prop14Text = 'https://vig.cdn.sos.ca.gov/2020/general/pdf/topl-prop14.pdf';
+const prop15Text = 'https://vig.cdn.sos.ca.gov/2020/general/pdf/topl-prop15.pdf';
+const prop16Text = 'https://vig.cdn.sos.ca.gov/2020/general/pdf/topl-prop16.pdf';
+const prop17Text = 'https://vig.cdn.sos.ca.gov/2020/general/pdf/topl-prop17.pdf';
+const prop18Text = 'https://vig.cdn.sos.ca.gov/2020/general/pdf/topl-prop18.pdf';
+const prop19Text = 'https://vig.cdn.sos.ca.gov/2020/general/pdf/topl-prop19.pdf';
+const prop20Text = 'https://vig.cdn.sos.ca.gov/2020/general/pdf/topl-prop20.pdf';
+const prop21Text = 'https://vig.cdn.sos.ca.gov/2020/general/pdf/topl-prop21.pdf';
+const prop22Text = 'https://vig.cdn.sos.ca.gov/2020/general/pdf/topl-prop22.pdf';
+const prop23Text = 'https://vig.cdn.sos.ca.gov/2020/general/pdf/topl-prop23.pdf';
+const prop24Text = 'https://vig.cdn.sos.ca.gov/2020/general/pdf/topl-prop24.pdf';
+const prop25Text = 'https://vig.cdn.sos.ca.gov/2020/general/pdf/topl-prop25.pdf';
 
 const Titles = [prop14Title, prop15Title, prop16Title, prop17Title, prop18Title, prop19Title, prop20Title, prop21Title, prop22Title, prop23Title, prop24Title, prop25Title];
 
@@ -60,9 +85,15 @@ const Yes = [prop14yes, prop15yes, prop16yes, prop17yes, prop18yes, prop19yes, p
 
 const No = [prop14no, prop15no, prop16no, prop17no, prop18no, prop19no, prop20no, prop21no, prop22no, prop23no, prop24no, prop25no];
 
+const Texts = [prop14Text, prop15Text, prop16Text, prop17Text, prop18Text, prop19Text, prop20Text, prop21Text, prop22Text, prop23Text, prop24Text, prop25Text];
+
+const Background = [prop14Back, prop15Back, prop16Back, prop17Back, prop18Back, prop19Back, prop20Back, prop21Back, prop22Back, prop23Back, prop24Back, prop25Back];
+
 export {
 	Titles,
 	Keywords,
 	Yes,
-	No
+	No,
+	Texts,
+	Background
 }
--- src/components/PropPage.jsx
+++ src/components/PropPage.jsx
@@ -3,10 +3,42 @@ import { useHistory, useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Heading } from './Heading';
 import { Footer } from './Footer';
-import { Titles, Keywords, Yes, No } from './PropContent';
+import { Titles, Keywords, Yes, No, Texts, Background } from './PropContent';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../scss/PropPage.scss';
+
+const NavButton = ({bool, func, icon}) => {
+	return (<button className="button prev" disabled={bool} onClick={func}>
+		<FontAwesomeIcon icon={icon}/>
+	</button>);
+}
+
+const KeyWords = ({k}) => {
+	return (<div className={"keywords"}>keywords: {k}</div>);
+}
+
+const BackgroundText = ({background}) => {
+	return (<div className={"backgroundwrapper"}>
+		<div className="propbackground">
+			{background}
+		</div>
+	</div>);
+}
+
+const VoteProp = ({vote, votetext, page}) => {
+	return (<div className={"prompt"}>
+			Voting
+			<div className={"bolder"}>{vote}</div>
+			{votetext}
+		</div>);
+}
+
+const FullTextLink = ({text}) => {
+	return (<div className={"fulltext"}>
+		<a href={text}>Full Proposition Text</a>
+	</div>);
+}
 
 const PropPage = () => {
 
@@ -14,6 +46,14 @@ const PropPage = () => {
 	const params = useParams();
 	let page = params["prop"]
 	page = parseInt(page.substring(1));
+
+	const [currPos, updateCurrPos] = useState([]);
+
+	const back = Background[page-14];
+	const text = Texts[page-14];
+	const key = Keywords[page-14];
+	const voteyes = Yes[page-14];
+	const voteno = No[page-14];
 
 	let prevActive;
 	(page === 14) ? prevActive = true : prevActive = false;
@@ -30,20 +70,31 @@ const PropPage = () => {
 			history.push(`/props/:${page+1}`);
 	}
 
+	const start = async (e) => {
+		console.log(e.changedTouches[0]);
+		await updateCurrPos([e.changedTouches[0].screenX, e.changedTouched[0].screenY]);
+	}
+
+	const calcDifference = async (e) => {
+		console.log(e.changedTouches[0]);
+		console.log(`xdiff: ${currPos[0] - e.changedTouches[0].screenX}`);
+		console.log(`ydiff: ${currPos[1] - e.changedTouches[0].screenY}`);
+		await updateCurrPos([e.changedTouches[0].screenX, e.changedTouches[0].screenY]);
+	}
+
 	return (<><Heading/>
 		<div className="proposition">
 		  <div className="linkblock">
-			<button className="button prev" disabled={prevActive} onClick={prev}>
-				<FontAwesomeIcon icon={faChevronLeft}/>
-			</button>
-			  <div className={"content"}>
+		    <NavButton bool={prevActive} func={prev} icon={faChevronLeft}/>
+			  <div className={"content"} onTouchStart={start} onTouchMove={calcDifference}>
 			    <div className={"proptitle"}>Proposition {page}: {Titles[page-14]}</div>
-				<div>{Yes[page-14]}</div>
-				<div>{No[page-14]}</div>
+				<KeyWords k={key}/>
+				{back ? <BackgroundText background={back}/> : ''}
+				<VoteProp vote={"YES"} votetext={voteyes} page={page}/>
+			  	<VoteProp vote={"NO"} votetext={voteno} page={page}/>
+				<FullTextLink textlink={text}/>
 			  </div>
-			<button className="button next" disabled={nextActive} onClick={next}>
-				<FontAwesomeIcon icon={faChevronRight}/>
-			</button>
+		    <NavButton bool={nextActive} func={next} icon={faChevronRight}/>
 		  </div>
 		</div>
 	<Footer/></>);
